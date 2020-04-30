ee494e9fade6fa441d55563de436fa215b4d5b37
clover-leaf <42005590+clover-leaf@users.noreply.github.com>
2020-04-30 18:39:21 +0700

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
-
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://mixxim:thanghay@react-blog-e9yii.mongodb.net/test?retryWrites=true&w=majority', {
-	useNewUrlParser: true})
-	.then(()=>console.log('DB connected'))
-	.catch(err => console.error(err))
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
+
+const { User } = require('./models/user')
+
+mongoose.connect(config.mongoURI, {
+	useNewUrlParser: true}).then(()=>console.log('DB connected'))
+							.catch(err => console.error(err))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.post('/api/users/register', (req, res) => {
+	const user = new User(req.body)
+	user.save((err, userData) => {
+		if(err) return res.json({success: false, err})
+		return res.status(200).json({
+			success: true
+		})
+	})	
+})
 
 app.get('/', (req, res)=>{
-	res.send('hello world');
+	res.send('hi world');
 })
 
 app.listen(1006);
